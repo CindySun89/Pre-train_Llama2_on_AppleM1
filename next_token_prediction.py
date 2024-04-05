526fae94f50f23b0fcc9b019eebdc7c836135013
CindySun <cindyluvcoding@gmail.com>
2024-04-05 11:22:01 -0400

    do_inference portion of the codes are packaged into a function. Command line inputs are enabled using python argparse.

--- next_token_prediction.py
+++ next_token_prediction.py
@@ -4,10 +4,14 @@ import json
 import numpy as np
 import random
 import sys
+import argparse
 import torch
 import torch.nn.functional as F
 import torch.nn as nn
 from sentencepiece import SentencePieceProcessor
+import llama
+from llama.model import ModelArgs, Transformer
+from llama import Llama, Dialog
 
 ## https://pytorch.org/xla/release/2.1/index.html#pytorch-on-xla-devices
 # import torch_xla.core.xla_model as xm
@@ -18,13 +22,12 @@ class bcolors:
     ENDC = '\033[0m'
     UNDERLINE = '\033[4m'
 
+# Path to the Llama2 model on local machine that was checked out from Meta's GitHub
+#llama2_path = '../llama'
+#sys.path.append(llama2_path)
 
-llama2_path = '../llama'
-sys.path.append(llama2_path)
-import llama
-from llama.model import ModelArgs, Transformer
-from llama import Llama, Dialog
 
+# Define utility function to perform top-p sampling
 def sample_top_p(probs, p):
   """
   Perform top-p (nucleus) sampling on a probability distribution.
@@ -50,35 +53,41 @@ def sample_top_p(probs, p):
   next_token = torch.gather(probs_idx, -1, next_token)
   return next_token
 
+# The main function for pre-training a Llama2 model on a text corpus
 def main(
-    ckpt_dir: str,
+    param_dir: str,
     tokenizer_path: str,
-    temperature: float = 0.6,
+    temperature: float = 0.3,
     top_p: float = 0.9,
     max_seq_len: int = 128,
     max_gen_len: int = 64,
     max_batch_size: int = 4,
-    traindata_path: str = "the_prince.txt"
+    traindata_path: str = ""
 ):
     """
-    Entry point of the program for generating text using a pretrained model.
+    Pre-train a Llama2 model using local training data in the form of a text file.
 
     Args:
-        ckpt_dir (str): The directory containing checkpoint files for the pretrained model.
+        param_dir (str): The directory containing model configuration json file.
         tokenizer_path (str): The path to the tokenizer model used for text encoding/decoding.
         temperature (float, optional): The temperature value for controlling randomness in generation.
-            Defaults to 0.6.
+            Defaults to 0.3.
         top_p (float, optional): The top-p sampling parameter for controlling diversity in generation.
             Defaults to 0.9.
         max_seq_len (int, optional): The maximum sequence length for input prompts. Defaults to 128.
         max_gen_len (int, optional): The maximum length of generated sequences. Defaults to 64.
         max_batch_size (int, optional): The maximum batch size for generating sequences. Defaults to 4.
+        traindata_path (str, optional): The path to the training data file. 
     """
-
+    # Use SentencePiece to encode/decode the text data
     sp_model = SentencePieceProcessor(model_file=tokenizer_path)
-
+    # Load the train data
+    if traindata_path == "":
+       print("No training data path provided. Exiting.")
+       return
+  
     with open(traindata_path, 'r') as f:
-        data = f.read()
+      data = f.read()
 
     all_tokens = []
     text = data.strip()
@@ -86,14 +95,15 @@ def main(
     tokens = [sp_model.bos_id()] + tokens
     all_tokens.extend(tokens)
 
-    # convert to uint16 np array
-    import numpy as np
+    # convert to uint16 np array to save space when saving to disk
+    #import numpy as np
     all_tokens = np.array(all_tokens, dtype=np.uint16)
-    with open('the_prince.bin', 'wb') as f:
+    with open('traindata.bin', 'wb') as f:
         f.write(all_tokens.tobytes())
 
-    print("[START] training tiny llama2 on Apple M1 processor")
-    with open(Path(ckpt_dir) / "params.json", "r") as f:
+    print("[START] Training tiny Llama2 model on Apple M1 processor:")
+    # Load the Llama2 Transformer model
+    with open(Path(param_dir) / "params.json", "r") as f:
       params = json.loads(f.read())
 
     model_config = ModelArgs(**params)
@@ -107,7 +117,7 @@ def main(
 
     # Load training samples
     BS = model_config.max_batch_size
-    train_fn = "the_prince.bin"
+    train_fn = "traindata.bin"
     m = np.memmap(train_fn, dtype=np.uint16, mode='r')
     max_seq_len = model_config.max_seq_len
     num_batches = len(m) // max_seq_len
@@ -115,7 +125,8 @@ def main(
     assert num_batches > 0
 
     ixs = list(range(num_batches))
-    random.Random(42).shuffle(ixs)
+    random_seed = 42
+    random.Random(random_seed).shuffle(ixs)
     train_data = []
     for ix in ixs:
         start = ix * max_seq_len
@@ -126,13 +137,14 @@ def main(
         train_data.append((x, y))
 
     print("Train data size = ", len(train_data))
-
+    # Start model training
     model.train()
-    optimizer = torch.optim.Adam(model.parameters(), lr=0.0005)
+    learning_rt = 0.0005
+    optimizer = torch.optim.Adam(model.parameters(), lr=learning_rt)
     lossFn = nn.CrossEntropyLoss(ignore_index=-1)
     total_loss = 0
     count = 0
-    n_epochs = 5
+    n_epochs = 200
     for epoch in range(n_epochs):
         for X, Y in train_data:
           optimizer.zero_grad()
@@ -146,11 +158,16 @@ def main(
           optimizer.step()
           total_loss += loss.item()
           # xm.mark_step()
-          if count % 367 == 0:
+          if count % 6 == 0:
             print("data point: ", count, ", loss = ", loss.item())
-            avg_loss = total_loss / len(train_data)
+            #avg_loss = total_loss / len(train_data)
+            if count > 0:
+              avg_loss = total_loss / count
+            else:
+              avg_loss = total_loss 
+            
+            print("epoch ", epoch, ": average loss = ", avg_loss)
             # checkpointing model
-            print("epoch ", epoch, ": loss = ", avg_loss)
             torch.save({'epoch': epoch,
                         'model_state_dict': model.state_dict(),
                         'optimizer_state_dict': optimizer.state_dict(),
@@ -158,42 +175,46 @@ def main(
                         }, "model_ckpt.pt")
           count += 1
 
-if __name__ == "__main__":
-  ckpt_dir = "llama2-tiny"
-  # tokenizer_path = "tokenizer.model"
-  tokenizer_path = "codellama7b_tokenizer.model"
-  device = "cpu"
 
-  # Toggle for pre-training or inference
-  do_inference: bool = False
-  do_training: bool = not do_inference
+def do_inference(
+    param_dir: str,
+    tokenizer_path: str,
+    ckpt_path: str,
+    prompt_text: str,
+    temperature: float = 0.3,
+    top_p: float = 0.9,
+    device: str = "cpu"
+):
+    """
+    Making inference from a prompt using pre-trained Llama2 model.
 
-  if do_training:
-    # Task 0: Train llama2 to read <<the prince>>.
-    # main(ckpt_dir="llama-2-tiny", tokenizer_path=tokenizer_path)
+    Args:
+        param_dir (str): The directory containing the model configration json file.
+        tokenizer_path (str): The path to the tokenizer model used for text encoding/decoding.
+        ckpt_path (str): The directory containing checkpoint files for the pretrained model.
+        prompt_text (str): The prompt text for generating the next token.
+        temperature (float, optional): The temperature value for controlling randomness in generation.
+            Defaults to 0.3.
+        top_p (float, optional): The top-p sampling parameter for controlling diversity in generation.
+            Defaults to 0.9.
+        device (str, optional): The device to run inference on. Defaults to "cpu".  
+    """
 
-    # Task 1: Train llama2 to code like llvm
-    main(ckpt_dir="llama2-tiny", tokenizer_path=tokenizer_path, traindata_path="samplecode.txt")
-
-  elif do_inference:
-    # top-p sampling temperature:
-    temperature = 0.6
-    top_p = 0.9
-
-    # Load from model checkpoint and see what llama has learnt.
+    # Load from model checkpoint and see what Llama has learnt.
     # TODO: implement model beam search sampling
-    with open(Path(ckpt_dir) / "params.json", "r") as f:
+    with open(Path(param_dir) / "params.json", "r") as f :
       params = json.loads(f.read())
 
-    checkpoint = torch.load("model_ckpt.pt")
+    checkpoint = torch.load(ckpt_path)
     model_config = ModelArgs(**params)
     model = Transformer(model_config)
     model.load_state_dict(checkpoint['model_state_dict'])
     sp_model = SentencePieceProcessor(model_file=tokenizer_path)
 
-    max_seq_len = model_config.max_seq_len
-    prompt_text= "import math\ndef gridsearch("
-    raw_tokens = sp_model.encode(prompt_text)
+    #max_seq_len = model_config.max_seq_len
+
+    # Encode Prompt Text
+    raw_tokens = sp_model.encode(prompt_text) # Encode the prompt text
     #seed_bpe = [sp_model.decode(token) for token in raw_tokens]
     #print(prompt_text)
     #print("========= after tokenization =========")
@@ -246,7 +267,39 @@ if __name__ == "__main__":
     # Generated text from sampling is underlined.
     print(prompt_text + bcolors.UNDERLINE + sampled_text[0] + bcolors.ENDC)
 
-  else:
-    # Shouldn't reach here.
-    assert False, "Set do_training or do_inference"
 
+# Set up the model
+parser = argparse.ArgumentParser(description='Tiny Llama2 Pre-training or Inferencing.')
+parser.add_argument('doinference', type = bool,const=None, default=None,\
+                    help = 'Toggle indicating doing inference (True) or pre-training (False)')  # do_inference (True) or do_training (False) flag
+parser.add_argument('-param_dir', default = 'llama2-tiny', help = 'path to model configuration jason file')           
+parser.add_argument('-tokenizer_path', default = 'codellama7b_tokenizer.model')
+parser.add_argument('-device', default = 'cpu')           
+parser.add_argument('-ckpt_path', default = 'model_ckpt.pt')  
+parser.add_argument('-prompt_text', default = "How to assess fairness assessment?")
+parser.add_argument('-traindata', default = "sr1107a1.txt") 
+args = parser.parse_args()
+#print(args.inference, args.param_dir, args.tokenizer_path, args.device, args.ckpt_path, args.prompt_text, args.traindata)
+print(args.doinference)
+if __name__ == "__main__":
+  #ckpt_dir = "llama2-tiny"
+  #tokenizer_path = "codellama7b_tokenizer.model"
+  #device = "cpu"
+
+  # Toggle for pre-training or inference!!! Set do_inference: bool to False for pre-training and True for inference
+  #do_inference: bool = False
+  #do_training: bool = not do_inference
+
+  if args.doinference:
+    # Task: Generate code from prompt text
+    #prompt_text= "from sklearn.model_selection import GridSearchCV\ndef test_gridsearch("
+    #prompt_text = "with open(filepath, "
+    #promt_text = "How to assess fairness assessment?"
+    do_inference(param_dir=args.param_dir,tokenizer_path=args.tokenizer_path, ckpt_path=args.ckpt_path,\
+                prompt_text=args.prompt_text, device = args.device)
+  else:
+    # Task: Train llama2 to code Python
+    main(param_dir=args.param_dir, tokenizer_path=args.tokenizer_path, traindata_path=args.traindata)
+  #else:
+    # Shouldn't reach here.
+    #assert False, "Set do_training or do_inference"
